2f721af9d1ef593e298b33cc2df7b900205353a7
jereme <jereme.daniels@gmail.com>
2019-11-22 14:10:26 -0500

    js not working the same as local

--- js/main.js
+++ js/main.js
@@ -28,7 +28,13 @@ function loadBanner() {
     let bottom = $el.position().top + $el.offset().top + $el.outerHeight(true);
     // $("#landing-top-spacer").css({ height: `${Math.floor(bottom)}px`});
     // $(".banner-text").css({ height: `${Math.floor(bottom)}px` });
-    $(".banner-text").css({ height: `${bottom}px` });
+    // $(".banner-text").css({ height: `${bottom}px` });
+    if (desktop.matches) {
+        $(".banner-text").css({ height: `800px` });
+    }else {
+        $(".banner-text").css({ height: `400px` });
+    }
+    
 }
 function menu(){
     const opacity = $(".mobile-menu").css('opacity');
